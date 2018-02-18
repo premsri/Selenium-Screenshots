ed34e8812b6142105efd7d9544a8080ce6b4f7b3
premsri <premsri2504@gmail.com>
2018-02-18 17:32:48 +0530

    updated commit

--- GmailSign.java
+++ GmailSign.java
@@ -6,14 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class GmailSign {
-
-	private static TimeUnit Seconds;
 
 	public static void main(String[] args) throws Throwable {
 		System.setProperty("webdriver.chrome.driver",
