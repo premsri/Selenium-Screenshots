bee92f114264b9b4346530a628b233512d89db4f
premsri <premsri2504@gmail.com>
2018-02-18 17:30:06 +0530

    updated gmail sign

--- GmailSign.java
+++ GmailSign.java
@@ -7,39 +7,53 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GmailSign {
+
+	private static TimeUnit Seconds;
 
 	public static void main(String[] args) throws Throwable {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\ELCOT\\javapracctise\\SeleniumPractise\\lib\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://accounts.google.com/ServiceLogin?#identifier");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.google.com");
+		driver.navigate().to("https://accounts.google.com/ServiceLogin?#identifier");
+		driver.getTitle();
 		WebElement email = driver.findElement(By.xpath("//input[@id='identifierId']"));
 		email.sendKeys("premsri2504@gmail.com");
 		WebElement next = driver.findElement(By.xpath("//span[@class='RveJvd snByac']"));
 		next.click();
 		Thread.sleep(1000);
-		WebElement pass = driver.findElement(By.xpath("//input[@name='password']"));
-		pass.sendKeys("**********");
-		WebElement submit = driver.findElement(By.xpath("//span[@class='RveJvd snByac']"));
-		submit.click();
+		try {
+			WebElement pass = driver.findElement(By.xpath("//input[@name='password']"));
+			pass.sendKeys("04250400");
+		} catch (Throwable e) {
+				e.getMessage();
+		}
+		driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();
 		Thread.sleep(1000);
 		WebElement compose = driver.findElement(By.xpath("//a[@class='WaidBe']"));
 		compose.click();
 		WebElement compose1 = driver.findElement(By.xpath("//div[@class='T-I J-J5-Ji T-I-KE L3']"));
 		compose1.click();
 		Thread.sleep(1000);
-		driver.findElement(By.xpath("//textarea[@name='to']")).sendKeys("premsri2504@gmail.com");
+		driver.findElement(By.xpath("//textarea[@name='to']")).sendKeys("greensvelmurugank451@gmail.com");
 		Thread.sleep(1000);
+		WebElement cc = driver.findElement(By.xpath("//span[@class='aB gQ pE']"));
+		cc.click();
+		driver.findElement(By.xpath("(//textarea[@class='vO'])[2]")).sendKeys("premsri2504@gmail.com");
 		WebElement subject = driver.findElement(By.xpath("//input[@name='subjectbox']"));
 		subject.sendKeys("Feb 16th task - Automated Mail using Selenium ");
 		WebElement description = driver.findElement(By.xpath("//div[@class='Am Al editable LW-avf']"));
 		description.sendKeys("Dear Sir \n" +
 				"\n" 
-				+"Please find below url for screenshots for Feb 16th Task \n" + " \n " +		
-				"I tired something, if coding wrong pls correct me" + " \n" 
+				+"Please find below url for screenshots for Feb 16th Task \n" + " \n " +
+				"I tired something, if coding wrong please correct me" + " \n" 
 				+ "https://raw.githubusercontent.com/premsri/Selenium-Screenshots/master/Demoqa.png \n"
 				+ "https://raw.githubusercontent.com/premsri/Selenium-Screenshots/master/fbsignup.png \n"
 				+ "https://raw.githubusercontent.com/premsri/Selenium-Screenshots/master/gmailsignup.png \n"
@@ -47,6 +61,8 @@ public class GmailSign {
 				+ "https://raw.githubusercontent.com/premsri/Selenium-Screenshots/master/skype.png \n"
 				+ "https://raw.githubusercontent.com/premsri/Selenium-Screenshots/master/twitter.png \n"
 				+ "https://raw.githubusercontent.com/premsri/MyProject/master/Screenshot%20(21).png \n"
+				+ "below github url for gmailsignin task \n"
+				+"https://github.com/premsri/Selenium-Screenshots/blob/master/GmailSign.java  \n"
 				+ "Thank you \n"
 				+"Prem"
 
